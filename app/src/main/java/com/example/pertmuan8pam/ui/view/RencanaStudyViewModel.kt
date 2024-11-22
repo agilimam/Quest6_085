f1311e879bb44477bc62ea55eb6ff0b58805e1da
agilimam <agilimamalhafizi04@gmail.com>
2024-11-22 14:38:22 +0700

    Menambahkan MutableStateFlow untuk mengelola dan membagikan status KRS menggunakan RencanaStudy sebagai model data

--- app/src/main/java/com/example/pertmuan8pam/ui/view/RencanaStudyViewModel.kt
+++ app/src/main/java/com/example/pertmuan8pam/ui/view/RencanaStudyViewModel.kt
@@ -1,5 +1,13 @@
 package com.example.pertmuan8pam.ui.view
 
-data class RencanaStudyViewModel(){
+import androidx.lifecycle.ViewModel
+import com.example.pertmuan8pam.model.RencanaStudy
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class RencanaStudyViewModel:ViewModel(){
+    private val _krsState = MutableStateFlow(RencanaStudy())
+    val krsStateUi: StateFlow<RencanaStudy> =_krsState.asStateFlow()
 
 }
