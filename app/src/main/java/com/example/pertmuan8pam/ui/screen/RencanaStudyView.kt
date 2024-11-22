7a9f3811f2cdb09252db107e1d83b79bed976051
agilimam <agilimamalhafizi04@gmail.com>
2024-11-22 13:37:17 +0700

    menambahkan image di colom

--- app/src/main/java/com/example/pertmuan8pam/ui/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/pertmuan8pam/ui/screen/RencanaStudyView.kt
@@ -1,11 +1,14 @@
 package com.example.petemuan8.ui.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -13,7 +16,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.pertmuan8pam.R
 import com.example.pertmuan8pam.model.Mahasiswa
@@ -39,6 +44,11 @@ fun RencanaStudyView(
         Row (
             modifier = Modifier.fillMaxWidth().padding(16.dp), verticalAlignment = Alignment.CenterVertically
         ){
+            Image(painter = painterResource(id = R.drawable.umy),
+                contentDescription = "",
+                modifier = Modifier.clip(shape = CircleShape).size(50.dp
+                )
+            )
 
         }
 
